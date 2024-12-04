6265a61d310f6de6811cffc130d8db92a968ab8b
Ruoshui Zhang <isRuoshui-Zhang@outlook.com>
2024-12-04 20:47:12 +0800

    拆前稳定版

--- #globalfile.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#define ADMIN_FILE "admin.txt"
-#define CLERK_FILE "clerk.txt"
-#define CUSTOMER_FILE "customer.txt"
-#define ORDER_FILE "orders.txt"
-#define ROOM_FILE "rooms.txt"
--- #room.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "room.h"
-using namespace std;
-
-ostream& operator<<(ostream& cout, Room r)
-{
-	cout << r.roomId << '\t' << r.roomSize << '\t' << r.roomState << endl;
-	return cout;
-}
